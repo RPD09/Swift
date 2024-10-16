c361936276ab762e4c8a13b52c5d696a2eae4214
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-16 16:40:02 +0100

    Create Ex_24.swift

---
+++ Exercices/Week2/Ex_24.swift
@@ -0,0 +1,8 @@
+// Create a constant that receives a number
+// Create a constant isEvenNumber using the ternary operator to assign the value
+// Print the value of isEvenNumber
+
+let numero: Int = 2
+let isEvenNumber = numero % 2 == 0 ? true : false
+
+print(isEvenNumber) 
