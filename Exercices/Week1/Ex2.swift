8a53e0eda9af3139db1f48987fbe585db3fd6267
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-16 16:05:26 +0100

    Create Ex2.swift

---
+++ Exercices/Week1/Ex2.swift
@@ -0,0 +1,12 @@
+// Refactor the previous exercise using variables.
+// This code performs basic arithmetic operations using variables instead of constants.
+
+var val1: Int = 8
+var val2: Int = 2
+
+var sum: Int = val1 + val2
+var difference: Int = val1 - val2
+var product: Int = val1 * val2
+var quotient: Int = val1 / val2
+
+// Change: The use of 'let' has been replaced with 'var' to allow for variable reassignment.
