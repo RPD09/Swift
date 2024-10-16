55d7e6ef3ceaa6eeaa9e551027f4e9eab38d9013
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-16 16:03:53 +0100

    Create Ex1.swift

---
+++ Exercices/Week1/Ex1.swift
@@ -0,0 +1,11 @@
+// Create two integer constants with values 8 and 2.
+// Perform addition, subtraction, multiplication, and division.
+// Store each result in another constant.
+
+let const1: Int = 8
+let const2: Int = 2
+
+let sum = const1 + const2
+let subtract = const1 - const2
+let multiply = const1 * const2
+let divide = const1 / const2
