bba5b75ae34fbe72308ee458e7e818af4667de21
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-16 16:19:10 +0100

    Create Ex4.swift

---
+++ Exercices/Week2/Ex4.swift
@@ -0,0 +1,24 @@
+// Create a constant with a number between 1 and 7
+// Display a message with the name of the day of the week in full
+// Consider 1 as Sunday
+
+let number = 6
+
+switch number {
+case 1:
+    print("Sunday")
+case 2:
+    print("Monday")
+case 3:
+    print("Tuesday")
+case 4:
+    print("Wednesday")
+case 5:
+    print("Thursday")
+case 6:
+    print("Friday")
+case 7:
+    print("Saturday")
+default:
+    print("Invalid number. Please enter a number between 1 and 7.")
+}
