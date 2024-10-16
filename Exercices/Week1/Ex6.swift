a6ffa329f51f05d6e84813b6b6f5268070d06d8c
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-16 16:10:02 +0100

    Create Ex6.swift

---
+++ Exercices/Week1/Ex6.swift
@@ -0,0 +1,7 @@
+// Fix the message to include the name "Maria" in the welcome message.
+
+let client = "Maria"
+// Using string interpolation to include the client's name in the message.
+var message = "Welcome \(client)! Happy shopping!"
+
+print(message) 
