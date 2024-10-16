5288c9c435b4a7f26b6709ff4b7a901e91f9f745
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-16 16:39:22 +0100

    Create Ex_23.swift

---
+++ Exercices/Week2/Ex_23.swift
@@ -0,0 +1,12 @@
+// Create two integer constants for the birth year and the current year
+// Create a constant isAdult using the ternary operator to assign the value
+// Print the value of isAdult
+
+let anoDeNascimento = 2004
+let anoDeAtual = 2024
+
+
+let isAdult = (anoDeAtual - anoDeNascimento) >= 18 ? true : false
+
+
+print(isAdult)
