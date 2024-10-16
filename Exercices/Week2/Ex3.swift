6767b313abae045585d6049b1c756cbb55ddcea6
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-16 16:18:40 +0100

    Create Ex3.swift

---
+++ Exercices/Week2/Ex3.swift
@@ -0,0 +1,16 @@
+// Create a constant for the name and age
+// Create a constant isAdult that receives a boolean value
+// Display a message (with the person's name)
+// indicating that they can buy if they are an adult
+
+let name = "Rafael"
+let age = 10
+let isAdult: Bool = age >= 18
+
+print("\(name) is an adult? \(isAdult)")
+
+if isAdult {
+    print("\(name) can buy.")
+} else {
+    print("\(name) cannot buy.")
+}
