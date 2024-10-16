e4865a18a2e6dca3bd30ed5fb8ccf4860797628c
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-16 16:06:09 +0100

    Create Ex3.swift

---
+++ Exercices/Week1/Ex3.swift
@@ -0,0 +1,7 @@
+// Create constants for the number 10, the number 16, and the text "Joaquim".
+// The code prints a message including the name and the two numbers.
+
+let num1 = 10
+let num2 = 16
+let name = "Joaquim"
+print("\(name) was born on the \(num1)th at \(num2).")
