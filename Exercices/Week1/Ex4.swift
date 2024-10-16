a24e009c22b4bd18987fd53a42361b02e8d2c0df
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-16 16:08:09 +0100

    Create Ex4.swift

---
+++ Exercices/Week1/Ex4.swift
@@ -0,0 +1,6 @@
+// Create a constant for the year of birth, calculate the age in 2024, 
+// and assign it to a variable. Write the message: "You are XX years old."
+
+let birthYear = 2004
+let age = 2024 - birthYear
+print("You are \(age) years old.")
