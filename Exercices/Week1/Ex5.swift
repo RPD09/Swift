c5f4c2f8126dc4690c1512ad053efe03f5badb5d
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-16 16:08:50 +0100

    Create Ex5.swift

---
+++ Exercices/Week1/Ex5.swift
@@ -0,0 +1,7 @@
+// Store the year of birth and the year of death, 
+// and calculate how many years the person lived.
+
+let birthYear = 2004
+let yearOfDeath = 2017
+let yearsLived = yearOfDeath - birthYear
+print("The person lived for \(yearsLived) years.")
