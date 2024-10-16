005330b889f40328b3b26c6524a1dea46382522f
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-16 16:11:00 +0100

    Create Ex7.swift

---
+++ Exercices/Week1/Ex7.swift
@@ -0,0 +1,8 @@
+// Create a variable that receives the result of the following operation.
+// The result should be 12000 without changing operators or values.
+
+let thisYear = 2024
+// Adjusting the operation to ensure the result is 12000.
+let result: Int = 6 * 6 * (thisYear + 20 + 50 - 100)
+
+print(result)  // Output the final result
