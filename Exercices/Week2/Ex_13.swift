5081367d91d31ca460c00e815b471101f7c7202a
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-16 16:30:17 +0100

    Create Ex_13.swift

---
+++ Exercices/Week2/Ex_13.swift
@@ -0,0 +1,24 @@
+// Create a constant with a number between 1 and 7
+// Display a message with the name of the day of the week in full
+// Consider 1 as Sunday and use switch
+
+let value: Int = 5
+
+switch value {
+case 1:
+    print("Sunday")
+case 2:
+    print("Monday")
+case 3:
+    print("Tuesday")
+case 4:
+    print("Wednesday")
+case 5:
+    print("Thursday")
+case 6:
+    print("Friday")
+case 7:
+    print("Saturday")
+default:
+    print("Invalid value")
+}
