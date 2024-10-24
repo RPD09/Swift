e2d52fc54ccbd218cd45f9540dc6e465983659f1
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-24 18:35:14 +0100

    Create Ex1.swift

---
+++ Exercices/Week3/Ex1.swift
@@ -0,0 +1,10 @@
+// Exercise 1
+// Create arrays for honor roll students, movies, and prime numbers.
+// Remove the last movie from the list.
+
+var honorRoll2024: [String] = ["Rafael", "Linda"]
+
+var movies: [String] = ["Movie1", "Movie2", "Movie3"]
+movies.removeLast()
+
+var primeNumbers: [Int] = [2, 3, 5, 7, 11, 13, 17, 19]
