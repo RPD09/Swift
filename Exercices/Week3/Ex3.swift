600e98d34a096879b0e4753d1609a682c0614605
Rafael Dias <66210711+RPD09@users.noreply.github.com>
2024-10-24 18:36:24 +0100

    Create Ex3.swift

---
+++ Exercices/Week3/Ex3.swift
@@ -0,0 +1,7 @@
+// Create dictionaries for a city abbreviation, Roman numerals, and question-answer pairs.
+
+var city: Dictionary<String, String> = ["LIS": "Lisbon"]
+
+var romanNumerals: Dictionary<Int, String> = [1: "I", 2: "II", 3: "III", 4: "IV"]
+
+var questions: Dictionary<Int, Int> = [1: 2, 3: 4, 5: 6]
